8eb8d0e33bc80c2863d356d3152b5b238424af8a
MXCALLL <idbellam12@gmail.com>
2025-02-09 23:00:11 +0100

    norms optimisations & status

--- mandatory/pipex.c
+++ mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 18:07:28 by muidbell          #+#    #+#             */
-/*   Updated: 2025/02/09 11:42:04 by muidbell         ###   ########.fr       */
+/*   Updated: 2025/02/09 22:37:26 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,24 +43,28 @@ static void	open_files(char *infile, char *outfile, int *in, int *out)
 
 int	main(int ac, char *av[], char *env[])
 {
-	int		in;
-	int		out;
+	int		files[2];
+	int		status;
 	pid_t	pid1;
 	pid_t	pid2;
 	int		fd[2];
 
 	arg_check(ac);
-	open_files(av[1], av[4], &in, &out);
+	open_files(av[1], av[4], &files[0], &files[1]);
 	create_pipe(fd);
 	pid1 = create_process();
 	if (pid1 == 0)
-		exec_cmd1(fd, in, av[2], env);
+		exec_cmd1(fd, files[0], av[2], env);
 	pid2 = create_process();
 	if (pid2 == 0)
-		exec_cmd2(fd, out, av[3], env);
+		exec_cmd2(fd, files[1], av[3], env);
+	close(files[0]);
+	close(files[1]);
 	close(fd[0]);
 	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	waitpid(pid1, &status, 0);
+	waitpid(pid2, &status, 0);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
 	return (0);
 }
