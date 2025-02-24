4c82c21333e731134a6f8ec033c3ebb4fcc22516
MXCALLL <idbellam12@gmail.com>
2025-02-24 21:24:05 +0000

    update header

--- mandatory/pipex.h
+++ mandatory/pipex.h
@@ -6,7 +6,7 @@
 /*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 19:10:02 by muidbell          #+#    #+#             */
-/*   Updated: 2025/02/14 15:23:24 by muidbell         ###   ########.fr       */
+/*   Updated: 2025/02/19 15:03:30 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	**ft_split(char const *s, char c);
 void	exec_cmd1(int *fd, int in, char *cmd, char *env[]);
 void	exec_cmd2(int *fd, int out, char *cmd, char *env[]);
 void	create_pipe(int *fd);
-void	files_check(int in, int out);
+void	files_check(int in, int out, char *infile, char *outfile);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	free_array(char **array);
@@ -35,5 +35,6 @@ char	*valid_cmd(char **dirs, char *cmd);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
